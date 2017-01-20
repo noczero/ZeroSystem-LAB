3165f2e71833abbe78c22835d4c376ec81dc03c9
noczero <zeroonetm@gmail.com>
2017-01-20 15:54:45 +0700

    update database

--- aptrglab.sql
+++ aptrglab.sql
@@ -2,10 +2,10 @@
 -- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jan 19, 2017 at 02:33 PM
--- Server version: 10.1.8-MariaDB
--- PHP Version: 5.6.14
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2017 at 09:53 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,15 +30,16 @@ CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL,
-  `nama_admin` varchar(50) NOT NULL
+  `nama_admin` varchar(50) NOT NULL,
+  `email_admin` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_admin`) VALUES
-(1, 'admin', 'admin', 'Satrya Budi Pratama');
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama_admin`, `email_admin`) VALUES
+(1, 'admin', 'admin', 'Satrya Budi Pratama', 'zeroonetm@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -56,6 +57,13 @@ CREATE TABLE `asisten` (
   `divisi_asisten` varchar(10) NOT NULL,
   `notel_asisten` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `asisten`
+--
+
+INSERT INTO `asisten` (`id_asisten`, `nama_asisten`, `birthdate_asisten`, `alamat_asisten`, `foto_asisten`, `jurusan_asisten`, `divisi_asisten`, `notel_asisten`) VALUES
+(1, 'Satrya Budi Pratama', '2017-01-19', 'Adhyaksa V', 'sat.jpg', 'S1 Informatika', 'GCS', '082277009251');
 
 -- --------------------------------------------------------
 
@@ -77,7 +85,51 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama_barang`, `deskripsi_barang`, `foto_barang`, `jumlah_barang`, `kategori_barang`) VALUES
-(1, 'Komputer LAB', 'Processor : Core i5\r\nRAM : 2 GB\r\nHardisk : 1.5 T\r\nMotherboard : Asrock', 'komputer.jpg', 1, 'Elektronik');
+(1, 'Komputer LAB', 'Processor : Core i5\r\nRAM : 2 GB\r\nHardisk : 1.5 T\r\nMotherboard : Asrock', 'komputer.jpg', 1, 'Elektronik'),
+(2, 'AA', 'AA', 'AA', 2, 'AA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detil_pinjam`
+--
+
+CREATE TABLE `detil_pinjam` (
+  `id_detilpinjam` int(11) NOT NULL,
+  `id_pinjam` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detil_pinjam`
+--
+
+INSERT INTO `detil_pinjam` (`id_detilpinjam`, `id_pinjam`, `id_barang`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 1, 2),
+(4, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_pinjam` int(11) NOT NULL,
+  `id_asisten` int(11) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_pinjam`, `id_asisten`, `tgl_pinjam`, `tgl_kembali`) VALUES
+(1, 1, '2017-01-21', '2017-01-23'),
+(2, 1, '2017-01-23', '2017-01-31');
 
 --
 -- Indexes for dumped tables
@@ -102,6 +154,21 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
+-- Indexes for table `detil_pinjam`
+--
+ALTER TABLE `detil_pinjam`
+  ADD PRIMARY KEY (`id_detilpinjam`),
+  ADD KEY `id_pinjam` (`id_pinjam`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_pinjam`),
+  ADD KEY `id_user` (`id_asisten`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -114,12 +181,39 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `asisten`
 --
 ALTER TABLE `asisten`
-  MODIFY `id_asisten` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_asisten` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `detil_pinjam`
+--
+ALTER TABLE `detil_pinjam`
+  MODIFY `id_detilpinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detil_pinjam`
+--
+ALTER TABLE `detil_pinjam`
+  ADD CONSTRAINT `detil_fk1` FOREIGN KEY (`id_pinjam`) REFERENCES `peminjaman` (`id_pinjam`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detil_fk2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_fk1` FOREIGN KEY (`id_asisten`) REFERENCES `asisten` (`id_asisten`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
